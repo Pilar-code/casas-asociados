5736234deab1d3016309435b6d12c20506f5376f
Pilar-falcone <pilarfelicitasfalcone@gmail.com>
2024-08-19 14:46:09 -0300

    about completo
    primer subida de about responsive

--- about.php
+++ about.php
@@ -1,30 +1,78 @@
 <?php include('includes/header.php'); ?>
 
-<section id="quienes-somos">
-    <h2>Quiénes somos</h2>
-    <p>Somos especialistas en dar soluciones eficaces a grandes empresas, pymes y autónomos.</p>
-    <div class="team">
-        <div class="team-member">
-            <h3>Francisco Casas</h3>
-            <p>Director Gerente / Titular del despacho</p>
-        </div>
-        <div class="team-member">
-            <h3>Arantxa Casas</h3>
-            <p>Responsable dpto. Fiscal</p>
-        </div>
-        <div class="team-member">
-            <h3>Cristina Romeo</h3>
-            <p>Responsable dpto. Laboral</p>
-        </div>
-        <div class="team-member">
-            <h3>Marta García</h3>
-            <p>Responsable dpto. Fiscal-Contable</p>
-        </div>
-        <div class="team-member">
-            <h3>Nerea Casas</h3>
-            <p>Responsable dpto. Contabilidad</p>
+<section id="about">
+    <div>
+        <div class="about__title">
+                <h5>QUIÉNES SOMOS</h5>
+                <hr>
+                <img src="img/detalle.png" alt="">
+            </div>
+            <div class="about__subtitle">
+                <h2>Juntos
+                    <br>trabajamos
+                    <br>cada día
+                    <br>para brindar
+                    <br>soluciones
+                    <br>eficientes y
+                    <br>personalizadas
+                </h2>
+            </div>
+            <div class="about__example">
+                <p>
+                    En Casas & Asociados aunamos una dilatada
+                    experiencia como asesores y consultores de
+                    grandes empresas, pymes y autónomos, con
+                    jóvenes incorporaciones formados con las
+                    últimas reformas legislativas. 
+                </p>
+            </div>
         </div>
     </div>
 </section>
-
+<section id="quienes-somos">
+    <div class="img">
+        <img src="img/modern-workplace-with-laptop-table-office-mockup-design copia.png" alt="">
+    </div>
+    <div class="team">
+            <div class="team__position">
+                <div class="team-member">
+                    <h3>Francisco Casas</h3>
+                    <p>Director Gerente / Titular del despacho</p>
+                    <img src="img/icons8-teléfono-desconectado-48.png" alt="">
+                    <img src="img/icons8-nuevo-post-48.png" alt="">
+                    <img src="img/icons8-linkedin-60.png" alt="">
+                </div>
+                <div class="team-member">
+                    <h3>Arantxa Casas</h3>
+                    <p>Responsable dpto. Fiscal</p>
+                    <img src="img/icons8-teléfono-desconectado-48.png" alt="">
+                    <img src="img/icons8-nuevo-post-48.png" alt="">
+                    <img src="img/icons8-linkedin-60.png" alt="">
+                </div>
+                <div class="team-member">
+                    <h3>Cristina Romeo</h3>
+                    <p>Responsable dpto. Laboral</p>
+                    <img src="img/icons8-teléfono-desconectado-48.png" alt="">
+                    <img src="img/icons8-nuevo-post-48.png" alt="">
+                    <img src="img/icons8-linkedin-60.png" alt="">
+                </div>
+                <div class="team-member">
+                    <h3>Marta García</h3>
+                    <p>Responsable dpto. Fiscal-Contable</p>
+                    <img src="img/icons8-teléfono-desconectado-48.png" alt="">
+                    <img src="img/icons8-nuevo-post-48.png" alt="">
+                    <img src="img/icons8-linkedin-60.png" alt="">
+                </div>
+                <div class="team-member">
+                    <h3>Nerea Casas</h3>
+                    <p>Responsable dpto. Contabilidad</p>
+                    <img src="img/icons8-teléfono-desconectado-48.png" alt="">
+                    <img src="img/icons8-nuevo-post-48.png" alt="">
+                    <img src="img/icons8-linkedin-60.png" alt="">
+                </div>
+            </div>
+        <hr>
+    </div>
+</section>
+<?php include('includes/form.php'); ?>
 <?php include('includes/footer.php'); ?>
